c5b3427cff58df1893d006aef0fbdae56398175b
Gregory Bers <gregorybers@epiecs.be>
2025-03-25 21:42:54 +0100

    created global variables for grill. wifi and battery

--- lib/Global/Global.h
+++ lib/Global/Global.h
@@ -1,9 +1,24 @@
 #include <WebServer.h>
+#include <string>
 
 #include "Probe.h"
 
+// Grill settings
+extern std::string grill_name;
+extern std::string grill_uuid;
+extern std::string grill_firmware_version;
+
+// Battery
+extern int battery_percent;
+extern bool battery_charging;
+
+// Wifi
+extern bool wifi_connected;
+
+// Webserver for api
 extern WebServer webserver;
 
+// Probes
 extern Probe probe_1;
 extern Probe probe_2;
 extern Probe probe_3;
