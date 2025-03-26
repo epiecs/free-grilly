3ce2c41fe3e2582be7ccc838edc910bec4f7e2b3
Gregory Bers <gregorybers@epiecs.be>
2025-03-26 10:19:43 +0100

    documented the probe class

--- lib/Probes/Probe.h
+++ lib/Probes/Probe.h
@@ -16,6 +16,13 @@ private:
 	const int   MUX_SELECTOR_B                 =    25; // MUX pin B
 	const int   MUX_SELECTOR_C                 =    26; // MUX pin C
 
+	/**
+	 * @brief Selects the wanted probe via the MUX pinouts
+	 * 
+	 * @param probeNumber The probe number
+	 */
+	void select_probe(int probeNumber);
+
 public:
 
 	int number;											// The probe number
@@ -23,10 +30,34 @@ public:
 	int reference_celcius;								// Reference temperature in C from the datasheet
 	int reference_beta;									// Beta value from the datasheet
 
+	/**
+	 * @brief Construct a new Probe object
+	 * 
+	 * @param number 				The probe number
+	 * @param reference_kohm 		The kOhm value from the datasheet at the reference temperature
+	 * @param reference_celcius 	The reference temperature from the datasheet in Celcius
+	 * @param reference_beta 		The beta coefficient value from the datasheet
+	 */
 	Probe(int number = 1, int reference_kohm = 100, int reference_celcius = 25, int reference_beta = 4250);
 
-	void select_probe(int probeNumber);
+	/**
+	 * @brief Read the raw ADC value
+	 * 
+	 * @return * uint16_t Unsigned 16 bit value containing the raw voltage ratio
+	 */
 	uint16_t read_adc_value();
+
+	/**
+	 * @brief Read the current measured voltage at the ADC in relation to the reference voltage
+	 * 
+	 * @return float The measured voltage
+	 */
 	float read_adc_voltage();
+
+	/**
+	 * @brief Read the current voltage and use the probe settings to calculate the temperature
+	 * 
+	 * @return float The temperature in Celcius
+	 */
 	float calculate_temperature();
 };
