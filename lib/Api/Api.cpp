517b1bbc782a4560ad25b662ba65904f568f592a
Gregory Bers <gregorybers@epiecs.be>
2025-03-27 15:55:20 +0100

    updated get call for grill added initlal openapi spec

--- lib/Api/Api.cpp
+++ lib/Api/Api.cpp
@@ -16,40 +16,42 @@ void get_api_temperatures()
     jsondoc["firmware_version"] = grill_firmware_version;
     jsondoc["battery"]          = battery_percent;
     jsondoc["battery_charging"] = battery_charging;
-    jsondoc["connected"]        = wifi_connected;
+    jsondoc["wifi_connected"]   = wifi_connected;
+    jsondoc["wifi_ssid"]        = "WOZ";
+    jsondoc["wifi_signal"]      = "-61";
     
     JsonArray probeData = jsondoc["probes"].to<JsonArray>();
     
     JsonObject probeData_0 = probeData.add<JsonObject>();
-    probeData_0["probeId"] = 1;
+    probeData_0["probe_id"] = 1;
     probeData_0["temperature"] = probe_1.calculate_temperature();
     
     JsonObject probeData_1 = probeData.add<JsonObject>();
-    probeData_1["probeId"] = 2;
+    probeData_1["probe_id"] = 2;
     probeData_1["temperature"] = probe_2.calculate_temperature();
     
     JsonObject probeData_2 = probeData.add<JsonObject>();
-    probeData_2["probeId"] = 3;
+    probeData_2["probe_id"] = 3;
     probeData_2["temperature"] = probe_3.calculate_temperature();
     
     JsonObject probeData_3 = probeData.add<JsonObject>();
-    probeData_3["probeId"] = 4;
+    probeData_3["probe_id"] = 4;
     probeData_3["temperature"] = probe_4.calculate_temperature();
     
     JsonObject probeData_4 = probeData.add<JsonObject>();
-    probeData_4["probeId"] = 5;
+    probeData_4["probe_id"] = 5;
     probeData_4["temperature"] = probe_5.calculate_temperature();
     
     JsonObject probeData_5 = probeData.add<JsonObject>();
-    probeData_5["probeId"] = 6;
+    probeData_5["probe_id"] = 6;
     probeData_5["temperature"] = probe_6.calculate_temperature();
     
     JsonObject probeData_6 = probeData.add<JsonObject>();
-    probeData_6["probeId"] = 7;
+    probeData_6["probe_id"] = 7;
     probeData_6["temperature"] = probe_7.calculate_temperature();
     
     JsonObject probeData_7 = probeData.add<JsonObject>();
-    probeData_7["probeId"] = 8;
+    probeData_7["probe_id"] = 8;
     probeData_7["temperature"] = probe_8.calculate_temperature();
 
     jsondoc.shrinkToFit();
