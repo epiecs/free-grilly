009c2413eb421d2fa0fc32623fc5541c4b01bee0
Gregory Bers <gregorybers@epiecs.be>
2025-04-14 10:30:22 +0200

    added endpoints to GET and POST probes updated documentation

--- lib/Api/Api.cpp
+++ lib/Api/Api.cpp
@@ -11,7 +11,7 @@
 #include "Web.h"
 
 JsonDocument jsondoc;
-char buffer[1000];
+char buffer[1500];
 
 void setup_api_routes()
 {
@@ -89,18 +89,152 @@ void get_api_grill()
     probeData_7["connected"] = grill::probe_8.connected;
 
     jsondoc.shrinkToFit();
-
     serializeJson(jsondoc, buffer);
     web::webserver.send(200, "application/json", buffer);
 }
 
 void get_api_probes() 
 {
+    jsondoc.clear();
+
+    JsonObject doc_0 = jsondoc.add<JsonObject>();
+    doc_0["probe_id"] = 1;
+    doc_0["temperature"] = grill::probe_1.celcius;
+    doc_0["target_temperature"] = grill::probe_1.target_temperature;
+    doc_0["connected"] = grill::probe_1.connected;
+    doc_0["probe_type"] = grill::probe_1.type;
+    doc_0["reference_kohm"] = grill::probe_1.reference_kohm;
+    doc_0["reference_celcius"] = grill::probe_1.reference_celcius;
+    doc_0["reference_beta"] = grill::probe_1.reference_beta;
+
+    JsonObject doc_1 = jsondoc.add<JsonObject>();
+    doc_1["probe_id"] = 2;
+    doc_1["temperature"] = grill::probe_2.celcius;
+    doc_1["target_temperature"] = grill::probe_2.target_temperature;
+    doc_1["connected"] = grill::probe_2.connected;
+    doc_1["probe_type"] = grill::probe_2.type;
+    doc_1["reference_kohm"] = grill::probe_2.reference_kohm;
+    doc_1["reference_celcius"] = grill::probe_2.reference_celcius;
+    doc_1["reference_beta"] = grill::probe_2.reference_beta;
+
+    JsonObject doc_2 = jsondoc.add<JsonObject>();
+    doc_2["probe_id"] = 3;
+    doc_2["temperature"] = grill::probe_3.celcius;
+    doc_2["target_temperature"] = grill::probe_3.target_temperature;
+    doc_2["connected"] = grill::probe_3.connected;
+    doc_2["probe_type"] = grill::probe_3.type;
+    doc_2["reference_kohm"] = grill::probe_3.reference_kohm;
+    doc_2["reference_celcius"] = grill::probe_3.reference_celcius;
+    doc_2["reference_beta"] = grill::probe_3.reference_beta;
+
+    JsonObject doc_3 = jsondoc.add<JsonObject>();
+    doc_3["probe_id"] = 4;
+    doc_3["temperature"] = grill::probe_4.celcius;
+    doc_3["target_temperature"] = grill::probe_4.target_temperature;
+    doc_3["connected"] = grill::probe_4.connected;
+    doc_3["probe_type"] = grill::probe_4.type;
+    doc_3["reference_kohm"] = grill::probe_4.reference_kohm;
+    doc_3["reference_celcius"] = grill::probe_4.reference_celcius;
+    doc_3["reference_beta"] = grill::probe_4.reference_beta;
+
+    JsonObject doc_4 = jsondoc.add<JsonObject>();
+    doc_4["probe_id"] = 5;
+    doc_4["temperature"] = grill::probe_5.celcius;
+    doc_4["target_temperature"] = grill::probe_5.target_temperature;
+    doc_4["connected"] = grill::probe_5.connected;
+    doc_4["probe_type"] = grill::probe_5.type;
+    doc_4["reference_kohm"] = grill::probe_5.reference_kohm;
+    doc_4["reference_celcius"] = grill::probe_5.reference_celcius;
+    doc_4["reference_beta"] = grill::probe_5.reference_beta;
+
+    JsonObject doc_5 = jsondoc.add<JsonObject>();
+    doc_5["probe_id"] = 6;
+    doc_5["temperature"] = grill::probe_6.celcius;
+    doc_5["target_temperature"] = grill::probe_6.target_temperature;
+    doc_5["connected"] = grill::probe_6.connected;
+    doc_5["probe_type"] = grill::probe_6.type;
+    doc_5["reference_kohm"] = grill::probe_6.reference_kohm;
+    doc_5["reference_celcius"] = grill::probe_6.reference_celcius;
+    doc_5["reference_beta"] = grill::probe_6.reference_beta;
+
+    JsonObject doc_6 = jsondoc.add<JsonObject>();
+    doc_6["probe_id"] = 7;
+    doc_6["temperature"] = grill::probe_7.celcius;
+    doc_6["target_temperature"] = grill::probe_7.target_temperature;
+    doc_6["connected"] = grill::probe_7.connected;
+    doc_6["probe_type"] = grill::probe_7.type;
+    doc_6["reference_kohm"] = grill::probe_7.reference_kohm;
+    doc_6["reference_celcius"] = grill::probe_7.reference_celcius;
+    doc_6["reference_beta"] = grill::probe_7.reference_beta;
+
+    JsonObject doc_7 = jsondoc.add<JsonObject>();
+    doc_7["probe_id"] = 8;
+    doc_7["temperature"] = grill::probe_8.celcius;
+    doc_7["target_temperature"] = grill::probe_8.target_temperature;
+    doc_7["connected"] = grill::probe_8.connected;
+    doc_7["probe_type"] = grill::probe_8.type;
+    doc_7["reference_kohm"] = grill::probe_8.reference_kohm;
+    doc_7["reference_celcius"] = grill::probe_8.reference_celcius;
+    doc_7["reference_beta"] = grill::probe_8.reference_beta;
+
+    jsondoc.shrinkToFit();
+    serializeJson(jsondoc, buffer);
+    web::webserver.send(200, "application/json", buffer);
 
 }
 
 void post_api_probes() 
 {
+    if(web::webserver.hasArg("plain") == false) { web::webserver.send(400, "application/json", "{\"error\": \"empty body\"}"); return;}
+
+    std::string body = web::webserver.arg("plain").c_str();
+    DeserializationError err = deserializeJson(jsondoc, body);
+    if(err) { web::webserver.send(400, "application/json", "{\"error\": \"malformatted body\"}"); return;}
+
+    JsonObject post_data = jsondoc.as<JsonObject>();
+
+    for (JsonObject item : jsondoc.as<JsonArray>()) {
+
+        int    probe_id           = item["probe_id"];
+        float  target_temperature = item["target_temperature"];
+        String probe_type         = item["probe_type"];
+        int    reference_kohm     = item["reference_kohm"];
+        int    reference_celcius  = item["reference_celcius"];
+        int    reference_beta     = item["reference_beta"];
+
+        switch (probe_id){
+            case 1:
+                grill::probe_1.set_type(probe_type, reference_kohm, reference_celcius, reference_beta);
+                break;
+            case 2:
+                grill::probe_2.set_type(probe_type, reference_kohm, reference_celcius, reference_beta);
+                break;
+            case 3:
+                grill::probe_3.set_type(probe_type, reference_kohm, reference_celcius, reference_beta);
+                break;
+            case 4:
+                grill::probe_4.set_type(probe_type, reference_kohm, reference_celcius, reference_beta);
+                break;
+            case 5:
+                grill::probe_5.set_type(probe_type, reference_kohm, reference_celcius, reference_beta);
+                break;
+            case 6:
+                grill::probe_6.set_type(probe_type, reference_kohm, reference_celcius, reference_beta);
+                break;
+            case 7:
+                grill::probe_7.set_type(probe_type, reference_kohm, reference_celcius, reference_beta);
+                break;
+            case 8:
+                grill::probe_8.set_type(probe_type, reference_kohm, reference_celcius, reference_beta);
+                break;
+            default:
+                break;
+        }
+    }
+
+    config::config_helper.save_probes();
+    // If all is ok just send the current settings
+    get_api_probes();
 
 }
 
@@ -130,6 +264,7 @@ void get_api_settings()
     // jsondoc["alarm_degrees_before"]     = 5;
     // jsondoc["alarm_on_wifi_disconnect"] = true;
     // jsondoc["auto_shutdown_time"]       = 180;
+    // jsondoc["mqtt_broker"]              = 180; //TODO add mqtt broker to sqlite
 
     jsondoc.shrinkToFit();
 
