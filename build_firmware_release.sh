9bdcfb141c08917d3249d546d45f38cd4eec8a9d
Gregory Bers <gregorybers@epiecs.be>
2025-05-18 17:39:09 +0200

    build script now also bumps the firmware version in Settings.h

--- build_firmware_release.sh
+++ build_firmware_release.sh
@@ -1,9 +1,18 @@
 #! /usr/bin/env bash
 
+# Bump the version in settings.h
+search="^    String grill_firmware_version.*"
+replace="    String grill_firmware_version       = \"$(date +%y.%m.%d)\";"
+
+sed -i "" "s#${search}#${replace}#" src/Settings.h
+
+# Build
 ~/.platformio/penv/bin/platformio run
 
+# Copy ota update
 cp .pio/build/esp32dev/firmware.bin dist/free-grilly-$(date +%Y-%m-%d)-ota.bin
 
+# Build full flash firmware including partitions, bootloader,...
 esptool.py --chip esp32 merge_bin -o dist/free-grilly-$(date +%Y-%m-%d)-full.bin \
   --flash_mode dio --flash_size 4MB \
   0x1000 .pio/build/esp32dev/bootloader.bin \
