124afaabd35defbe4bd8eb4e5af6aeeee85bc5cb
Gregory Bers <gregorybers@epiecs.be>
2025-06-01 21:40:33 +0200

    added binaries firmware bump in settings

--- src/Settings.h
+++ src/Settings.h
@@ -54,7 +54,7 @@ namespace config{
 
     String grill_name                   = "";
     String grill_uuid                   = "";
-    String grill_firmware_version       = "25.05.30";
+    String grill_firmware_version       = "25.06.01";
 
     String temperature_unit             = "celcius";
     bool beep_enabled                   = true;
