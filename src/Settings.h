d4e511df0b5504868e986705b37ee8f1dd5b729f
Gregory Bers <gregorybers@epiecs.be>
2025-06-14 19:59:40 +0200

    bumped firmware version and created new release

--- src/Settings.h
+++ src/Settings.h
@@ -54,7 +54,7 @@ namespace config{
 
     String grill_name                   = "";
     String grill_uuid                   = "";
-    String grill_firmware_version       = "25.06.01";
+    String grill_firmware_version       = "25.06.14";
 
     String temperature_unit             = "celcius";
     bool beep_enabled                   = true;
