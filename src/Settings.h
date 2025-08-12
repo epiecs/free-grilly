5585c14132ab2421ca9c827f57ade5fb8e93c2bf
Jeroen <Jeroen@questum.be>
2025-08-12 13:59:16 +0200

    bumped firmware version and created new beta release

--- src/Settings.h
+++ src/Settings.h
@@ -54,7 +54,7 @@ namespace config{
 
     String grill_name                   = "";
     String grill_uuid                   = "";
-    String grill_firmware_version       = "25.08.05";
+    String grill_firmware_version       = "25.08.12";
 
     String temperature_unit             = "celcius";
     bool beep_enabled                   = true;
